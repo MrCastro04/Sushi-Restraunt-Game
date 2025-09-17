7b6385204462841fc0148097223b18c88a7436e8
MrCastro04 <slavamora684@gmail.com>
2025-09-17 17:38:25 +0300

    сделал имитацию щаагрузки у эмплоера

---
+++ Sushi bar Game/Assets/Modules/Features/UI/Billboard/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Modules.Features.UI.Billboard
+{
+    public class Billboard : MonoBehaviour
+    {
+        private Camera _cameraCmp;
+
+        private void Awake()
+        {
+            _cameraCmp = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 directionOffset = transform.position + _cameraCmp.transform.forward;
+
+            transform.LookAt(directionOffset);
+        }
+    }
+}
---
+++ Sushi bar Game/Assets/Modules/Features/UI/Circle Loading/Code/LoadingCircle.cs
@@ -0,0 +1,21 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingCircle : MonoBehaviour
+{
+    [SerializeField] private Image _circleImage;
+    [SerializeField] private float _maxFillAmount;
+
+    private float _currentFill;
+    
+    public async void RunImmitation(float immitationTime)
+    {
+        _circleImage.fillAmount = 0;
+        
+        await _circleImage.DOFillAmount(_maxFillAmount, immitationTime).AsyncWaitForCompletion();
+        
+        Debug.Log("Имитация закончилась");
+    }
+}
