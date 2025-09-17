a5aa2e1f4ee21d8fb780ffd7dcd3fd8f85251926
MrCastro04 <slavamora684@gmail.com>
2025-09-17 16:44:53 +0300

    сделал скрипты перемещения работника на кнопку, и почистил папки

---
+++ Sushi bar Game/Assets/Modules/Features/Characters/Base/Code/BaseCharacterMover.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Modules.Features.Characters.Base.Code
+{
+    public abstract class BaseCharacterMover : MonoBehaviour
+    {
+        public async UniTask MoveTo(Transform targetTransform, float timeToPosition)
+        {
+          UniTask moveTask = gameObject.transform.DOMove(targetTransform.position, timeToPosition).AsyncWaitForCompletion().AsUniTask();
+
+          UniTask rotateTask = gameObject.transform.DOLookAt(targetTransform.position, timeToPosition).AsyncWaitForCompletion().AsUniTask();
+
+          await UniTask.WhenAll(moveTask,rotateTask);
+
+          await gameObject.transform.DORotateQuaternion(targetTransform.rotation, 0.2f).AsyncWaitForCompletion();
+        }
+    }
+}
---
+++ Sushi bar Game/Assets/Modules/Features/Characters/Employer/Code/Employer.cs
@@ -0,0 +1,9 @@
+﻿using Modules.Features.Characters.Base.Code;
+
+namespace Modules.Features
+{
+    public class Employer : BaseCharacterMover
+    {
+        
+    }
+}
